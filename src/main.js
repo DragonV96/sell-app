2fb085397511b690a6533ce9594714d087c82e5a
glw <guolongwei@comtom.cn>
2019-12-27 18:21:41 +0800

    [add]添加商家页面商品、评论和商家三个选框组件及header组件

--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import App from './App';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
 
-Vue.config.productionTip = false
+import 'common/stylus/index.styl';
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+let app = Vue.extend(App);
+
+let router = new VueRouter({
+  linkActiveClass: 'active'
+});
+
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/ratings': {
+    component: ratings
+  },
+  '/seller': {
+    component: seller
+  }
+});
+
+router.start(app, '#app');
+
+router.go('/goods');
