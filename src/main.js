f22ab7ae9314e54a9daff896b3732b613284e266
holiday <510537547@qq.com>
2020-01-30 19:52:45 +0800

    [init]vue2.5重构应用，初始化项目

--- src/main.js
+++ src/main.js
@@ -1,34 +1,8 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import VueResource from 'vue-resource';
-import App from './App';
-import goods from 'components/goods/goods';
-import ratings from 'components/ratings/ratings';
-import seller from 'components/seller/seller';
+import Vue from 'vue'
+import App from './App.vue'
 
-import 'common/stylus/index.styl';
+Vue.config.productionTip = false
 
-Vue.use(VueRouter);
-Vue.use(VueResource);
-
-let app = Vue.extend(App);
-
-let router = new VueRouter({
-  linkActiveClass: 'active'
-});
-
-router.map({
-  '/goods': {
-    component: goods
-  },
-  '/ratings': {
-    component: ratings
-  },
-  '/seller': {
-    component: seller
-  }
-});
-
-router.start(app, '#app');
-
-// router.go('/goods');
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
