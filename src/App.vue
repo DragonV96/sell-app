0821ad5f2990d5027c99af0307d4fa2a752efe8d
holiday <510537547@qq.com>
2020-02-01 20:47:59 +0800

    [update]重构商品、评价和商家标签组件，添加cube-ui滑块的实时联动效果

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,77 @@
 <template>
   <div id="app">
     <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
+    </div>
   </div>
 </template>
 
 <script>
-import VHeader from 'components/v-header/v-header'
-import { getSeller } from 'api'
+  import VHeader from 'components/v-header/v-header'
+  import Goods from 'components/goods/goods'
+  import Ratings from 'components/ratings/ratings'
+  import Seller from 'components/seller/seller'
+  import Tab from 'components/tab/tab'
+  import { getSeller } from 'api'
 
-export default {
-  name: 'app',
-  data() {
-    return {
-      seller: {}
+  export default {
+    name: 'app',
+    data() {
+      return {
+        seller: {}
+      }
+    },
+    computed: {
+      tabs() {
+        return [
+          {
+            label: '商品',
+            component: Goods,
+            data: {
+              seller: this.seller
+            }
+          },
+          {
+            label: '评价',
+            component: Ratings,
+            data: {
+              seller: this.seller
+            }
+          },
+          {
+            label: '商家',
+            component: Seller,
+            data: {
+              seller: this.seller
+            }
+          }
+        ]
+      }
+    },
+    created() {
+      this._getSeller()
+    },
+    methods: {
+      _getSeller() {
+        getSeller().then((seller) => {
+          this.seller = seller
+        })
+      }
+    },
+    components: {
+      VHeader,
+      Tab
     }
-  },
-  created() {
-    this._getSeller()
-  },
-  methods: {
-    _getSeller() {
-      getSeller().then((seller) => {
-        this.seller = seller
-      })
-    }
-  },
-  components: {
-    VHeader
   }
-}
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
+  #app
+    .tab-wrapper
+      position: fixed
+      top: 136px
+      left: 0
+      right: 0
+      bottom: 0
 </style>
