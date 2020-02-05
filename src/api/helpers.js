d0137234cc11637fa946d64711eaab1c55972db1
holiday <510537547@qq.com>
2020-02-05 22:04:15 +0800

    [add]添加接口路由

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -1,10 +1,16 @@
 import axios from 'axios'
 
+const urlMap = {
+  development: 'localhost:8099',
+  production: '192.168.0.101/sell'
+}
+
+const baseUrl = urlMap[process.env.NODE_ENV]
 const ERROR_OK = 0
 
 export function get(url) {
-  return function (params) {
-    return axios.get(url, {
+  return function (params = {}) {
+    return axios.get(baseUrl + url, {
       params
     }).then((res) => {
       const { errno, data } = res.data
